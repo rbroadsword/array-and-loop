967bba5241b4a81a7966ec4eadfc01d8c30ed300
Ryan Broadsword <rbroadsword@gmail.com>
2021-11-22 19:22:43 -0800

    work with Patrick on roboger function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
 //Business Logic
 
-//trying to merge my two functions roboger and range
-//function roboger(range) {
-  // let number = range;
-  // let one = "beep";
-  //let two = "boop";
-  //let three = "won't you be my neighbor?";
-  //let result = [];
-  //console.log(number);
-//for (i = 0; i <= number.length; i++) {
-  //if (number.includes("1") && number.includes("3") === false && number.includes("2") === false){
-      //console.log("hey oney!")
-      //result = one; 
-    //}else if(number.includes("2") && number.includes("3") === false){
-      //console.log("dose gurl");
-      //result = two;
-    //}else if(number.includes("3")){
-      //console.log("Cr33p");
-      //result = three; 
-    //}
-  //}
-  //return result; 
-//}
-
 //original working functions 
 function range(userNumber){
   let myNumber = parseInt(userNumber);
@@ -35,28 +12,29 @@ for (let i = 0; i <= myNumber; i++) {
 }
 
 function roboger(returnRange) {
-  let input = returnRange; //change array numbers to string? .toStraing()
-  let input1 = input.toString(); 
-  console.log(typeof input1);
-  let one = "beep";
-  let two = "boop";
-  let three = "won't you be my neighbor?";
+  let input = returnRange;
+  console.log(typeof input);
+  let one = " beep ";
+  let two = " boop ";
+  let three = " won't you be my neighbor? ";
   let result = [];
-  console.log(input1);
-for (i = 0; i <= input1.length; i++) {
-  if (input1.includes("1") && input1.includes("3") === false && input1.includes("2") === false){
-      console.log("one gurl!")
-      result = one;
-      return result; 
-    }else if(input1.includes("2") && input1.includes("3") === false){
-      console.log("two gurl");
-      result = two;
-    }else if(input1.includes("3")){
-      console.log("3 gurl");
-      result = three; 
+  console.log(input);
+for (i = 0; i <= input.length - 1; i++) {
+  if (input[i].toString().includes("1") && input[i].toString().includes("3") === false && input[i].toString().includes("2") === false){
+      result.push(one);
+      console.log(result)
+    }else if(input[i].toString().includes("2") && input[i].toString().includes("3") === false){
+      result.push(two);
+      console.log(result)
+    }else if(input[i].toString().includes("3")){
+      result.push(three);
+      console.log(result)
+    }else {
+      result.push(i);
     }
   }
   return result; 
+  console.log(result)
 }
 
 
