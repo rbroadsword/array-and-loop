825b74c790b7f3592efc7d63b8508dd0888eee7f
Ryan Broadsword <rbroadsword@gmail.com>
2021-11-22 20:00:46 -0800

    delete several console.log's

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,11 @@ for (let i = 0; i <= myNumber; i++) {
 function roboger(returnRange) {
   let input = returnRange;
   console.log(typeof input);
-  let one = " beep ";
-  let two = " boop ";
-  let three = " won't you be my neighbor? ";
+  let one = " 'beep' ";
+  let two = " 'boop' ";
+  let three = " 'won't you be my neighbor?' ";
   let result = [];
+  let resultAsString = result.join(", ");//I am not sure how to get it to return this in the UI Logic?
   console.log(input);
 for (i = 0; i <= input.length - 1; i++) {
   if (input[i].toString().includes("1") && input[i].toString().includes("3") === false && input[i].toString().includes("2") === false){
@@ -30,11 +31,11 @@ for (i = 0; i <= input.length - 1; i++) {
       result.push(three);
       console.log(result)
     }else {
-      result.push(i);
+      result.push(i)
     }
   }
-  return result; 
-  console.log(result)
+  //return resultAsString; when I have this here, it will not return on my index.html, it shows correctly in the console though. 
+  return result
 }
 
 
@@ -42,13 +43,9 @@ for (i = 0; i <= input.length - 1; i++) {
 $(document).ready(function() {
   $("form#number-range").submit(function(event){
     event.preventDefault();
-    
-    console.log("button kinda works"); //runs to here
     const userNumber = $('#user-number').val(); //this works
     const returnRange = range(userNumber); //this works
-    //console.log(userNumber);
     const resultRoboger = roboger(returnRange); 
-    console.log(resultRoboger);
-    $('#result-output').html(resultRoboger); //this works! need to change to resultRoboger once I get that working.
+    $('#result-output').html(resultRoboger);
   });
 });
